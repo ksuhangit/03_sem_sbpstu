56e1f38e1d28a810ff1f117e478d8c49ea1602e9
ksuhangit <164862786+ksuhangit@users.noreply.github.com>
2025-01-30 16:22:44 +0300

    Update FileReadException.java

--- oop/lab3/FileReadException.java
+++ oop/lab3/FileReadException.java
@@ -1,5 +1,10 @@
 package Lab3;
 
+/**
+ * Класс FileReadException пользовательское исключение для обработки ошибок, 
+ * связанных с чтением файла.
+*/
+
 public class FileReadException extends Exception {
     public FileReadException(String message) {
         super(message);
