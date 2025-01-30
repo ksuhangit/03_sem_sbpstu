b83088d76fab3213949ac5b2b4837a7fd91416d6
ksuhangit <164862786+ksuhangit@users.noreply.github.com>
2025-01-30 16:53:04 +0300

    Update InvalidFileFormatException.java

--- oop/lab3/InvalidFileFormatException.java
+++ oop/lab3/InvalidFileFormatException.java
@@ -1,5 +1,10 @@
 package Lab3;
 
+/**
+ * Класс InvalidFileFormatException пользовательское исключение для обработки ошибок, 
+ * связанных с недопустимым форматом файла. 
+ */
+
 public class InvalidFileFormatException extends Exception {
     public InvalidFileFormatException(String message) {
         super(message);
