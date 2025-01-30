355a9338eb35bd6fafe43968550b33de9c0c7e8d
ksuhangit <164862786+ksuhangit@users.noreply.github.com>
2025-01-30 16:52:21 +0300

    Update IDictionary.java

--- oop/lab3/IDictionary.java
+++ oop/lab3/IDictionary.java
@@ -1,5 +1,10 @@
 package Lab3;
 
+/**
+ * Интерфейс IDictionary определяет контракт для работы со словарем.
+ * Он обеспечивает функциональность перевода текста и загрузки словарных данных из файла.
+ */
+
 public interface IDictionary {
     String translate(String text);
     void loadDictionary(String filePath) throws FileReadException, InvalidFileFormatException;
